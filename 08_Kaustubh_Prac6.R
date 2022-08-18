3254fa6dc9bfa1121cc789e65eb1d4e21fcd2623
Sipher2003 <107745719+Sipher2003@users.noreply.github.com>
2022-08-18 23:14:02 +0530

    Add files via upload

---
+++ 08_Kaustubh_Prac6.R
@@ -0,0 +1,46 @@
+install.packages("e1071")          
+library("e1071") 
+#1
+x<-c(19.09, 19.55, 17.89, 17.73, 25.15, 27.27, 25.24, 21.05, 21.65, 20.92, 22.61, 15.71, 22.04, 22.60,24.25)
+print(skewness(x))
+print(kurtosis(x))
+hist(x)
+
+#2
+
+#3
+v<-c(88, 95, 92, 97, 96, 97, 94, 86, 91, 95, 97, 88, 85, 76, 68)
+
+#4
+duration = faithful$eruptions
+duration
+moment(duration, order=3, center=TRUE)
+print(skewness(duration))
+print(kurtosis(duration))
+
+#5
+x1<-seq(0,12,by -1)
+pbinom(x1,12,0.2)
+
+#7
+
+#P(X=5)
+dbinom(5,7,0.3)
+#P(X<=3)
+pbinom(3,7,0.3)
+#p(X>3)
+pbinom(3,7,0.7)
+#P(X>4)
+pbinom(4,7,0.7)
+
+
+#8
+
+#P(X=3)
+dnorm(3,15,0.3)
+#P(x=30)
+dnorm(30,15,0.3)
+#P(X>21)
+pmorm(30,15,0.7)
+
+
